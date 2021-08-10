677a89dea1ab4f029b55e89017417659d6e8ef19
iacopoerpichini <iacopo.erpichini@hotmail.it>
2021-08-10 08:42:38 +0200

    add the mapper structure

---
+++ plant-nursery/src/main/java/it/unifi/dinfo/swam/plantnursery/mapper/BaseMapper.java
@@ -0,0 +1,17 @@
+package it.unifi.dinfo.swam.plantnursery.mapper;
+
+import it.unifi.dinfo.swam.plantnursery.dto.BaseDto;
+import it.unifi.dinfo.swam.plantnursery.model.BaseEntity;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public abstract class BaseMapper<T extends BaseEntity, dtoT extends BaseDto> {
+
+    public abstract dtoT convert(T obj);
+
+    public List<dtoT> convert(Collection<T> list){
+        return list.stream().map(this::convert).collect(Collectors.toList());
+    }
+}
