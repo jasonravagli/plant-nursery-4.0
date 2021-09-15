ce3ea4862dbf16c549dc102a47f2047179379492
Iacopo Erpichini <iacopo.erpichini@hotmail.it>
2021-09-15 17:13:00 +0200

    position dao, controller partially implemented

---
+++ plant-nursery-nosql/src/main/java/it/unifi/dinfo/swam/plantnursery/nosql/dao/PositionByPlantDao.java
@@ -0,0 +1,7 @@
+package it.unifi.dinfo.swam.plantnursery.nosql.dao;
+
+import it.unifi.dinfo.swam.plantnursery.nosql.model.PositionByPlant;
+
+public class PositionByPlantDao extends BaseDao<PositionByPlant> {
+
+}
---
+++ plant-nursery-nosql/src/main/java/it/unifi/dinfo/swam/plantnursery/nosql/dao/PositionBySensorDao.java
@@ -0,0 +1,7 @@
+package it.unifi.dinfo.swam.plantnursery.nosql.dao;
+
+import it.unifi.dinfo.swam.plantnursery.nosql.model.PositionBySensor;
+
+public class PositionBySensorDao extends BaseDao<PositionBySensor>{
+
+}
